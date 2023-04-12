b14bb8920257412d47aa7b3d6b6303e735a31293
RaymondAlvin <108366163+RaymondAlvin@users.noreply.github.com>
2023-04-12 10:26:37 -0700

    Update newMotor.py

--- newMotor.py
+++ newMotor.py
@@ -16,49 +16,45 @@ STEPS = 200
 # The following pins are simply a suggestion. If you use different pins, update
 # the following code to use your chosen pins.
 
-# To use with CircuitPython and a microcontroller:
 coils = (
-    digitalio.DigitalInOut(board.D9),  # A1
-    digitalio.DigitalInOut(board.D10),  # A2
-    digitalio.DigitalInOut(board.D11),  # B1
-    digitalio.DigitalInOut(board.D12),  # B2
+    digitalio.DigitalInOut(board.D19),  # A1
+    digitalio.DigitalInOut(board.D26),  # A2
+    digitalio.DigitalInOut(board.D20),  # B1
+    digitalio.DigitalInOut(board.D21),  # B2
 )
-
-# To use with a Raspberry Pi:
-# coils = (
-#     digitalio.DigitalInOut(board.D19),  # A1
-#     digitalio.DigitalInOut(board.D26),  # A2
-#     digitalio.DigitalInOut(board.D20),  # B1
-#     digitalio.DigitalInOut(board.D21),  # B2
-# )
 
 for coil in coils:
     coil.direction = digitalio.Direction.OUTPUT
 
 motor = stepper.StepperMotor(coils[0], coils[1], coils[2], coils[3], microsteps=None)
 
-for step in range(STEPS):
+while True:
+    for step in range(STEPS):
     motor.onestep()
     time.sleep(DELAY)
 
-for step in range(STEPS):
-    motor.onestep(direction=stepper.BACKWARD)
-    time.sleep(DELAY)
 
-for step in range(STEPS):
-    motor.onestep(style=stepper.DOUBLE)
-    time.sleep(DELAY)
 
-for step in range(STEPS):
-    motor.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-    time.sleep(DELAY)
 
-for step in range(STEPS):
-    motor.onestep(style=stepper.INTERLEAVE)
-    time.sleep(DELAY)
+# for step in range(STEPS):
+#     motor.onestep(direction=stepper.BACKWARD)
+#     time.sleep(DELAY)
 
-for step in range(STEPS):
-    motor.onestep(direction=stepper.BACKWARD, style=stepper.INTERLEAVE)
-    time.sleep(DELAY)
+# for step in range(STEPS):
+#     motor.onestep(style=stepper.DOUBLE)
+#     time.sleep(DELAY)
+
+# for step in range(STEPS):
+#     motor.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+#     time.sleep(DELAY)
+
+# for step in range(STEPS):
+#     motor.onestep(style=stepper.INTERLEAVE)
+#     time.sleep(DELAY)
+
+# for step in range(STEPS):
+#     motor.onestep(direction=stepper.BACKWARD, style=stepper.INTERLEAVE)
+#     time.sleep(DELAY)
+
 
 motor.release()
